e17e93cf27af51fb03d7a80bcda71d4a222c29fd
mike.nguyen <515.miken@gmail.com>
2025-02-20 08:16:26 -0600

    Task 4, 5, 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,17 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+// Task 6
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+  if (users.find((user) => user.username === username)) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+  users.push({ username, password });
+  return res.status(201).json({ message: "User registered successfully" });
 });
 
 // Task 1
@@ -21,7 +28,7 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   if (books[isbn]) {
-    return res.json(books[isbn]);
+    return res.status(200).json(books[isbn]);
   } else {
     return res.status(404).json({ message: "Book not found" });
   }
@@ -40,22 +47,37 @@ public_users.get('/author/:author',function (req, res) {
   }
   
   if (authorBooks.length > 0) {  
-    res.send(authorBooks);  
+    res.status(200).json(authorBooks);  
   } else {
     res.status(404).send('No books found for author');  
   }
 });
 
+// Task 4
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  const filteredBooks = Object.values(books).filter(book => book.title.toLowerCase().includes(title));
+  if (filteredBooks.length > 0) {
+    return res.status(200).json(filteredBooks);
+  }
+  else {
+    return res.status(404).json({message: "Book not found"});
+  }
 });
 
+// Task 5
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const requestedIsbn = req.params.isbn;
+  const book = books[requestedIsbn];
+
+  if (book) {
+    const reviews = book.reviews;
+    res.status(200).json(reviews);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
